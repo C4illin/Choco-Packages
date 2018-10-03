a524483772af5d963de7781dda30b64e9df066c5
C4illin <20753603+C4illin@users.noreply.github.com>
2018-10-04 00:31:44 +0200

    initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName            = 'qalculate'
+  fileType               = 'MSI'
+  url                    = 'https://github.com/Qalculate/libqalculate/releases/download/v2.6.2/qalculate-2.6.2.msi'
+  checksum               = '19F8BF99AE72EB6DE4B91A5625667C331941CB5374B7B82741B1D8BDC4E55313'
+  checksumType           = 'sha256'
+  silentArgs             = '/qn'
+  validExitCodes         = @(0)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ update.ps1
@@ -0,0 +1,26 @@
+import-module au
+
+$releases = 'https://github.com/Qalculate/libqalculate/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $url     = $download_page.links | ? href -match '.msi$' | % href | select -First 1
+	$version = ($url -split '/' | select -last 1 -skip 1).substring(1)
+
+    @{
+        URL32   = 'https://github.com' + $url
+        Version = $version
+    }
+}
+
+update -ChecksumFor 32

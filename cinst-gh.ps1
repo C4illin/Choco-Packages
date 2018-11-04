fdfd9450d6cf573d2f184c6d4528687e7bca6e5f
C4illin <c@illin.cf>
2018-11-04 19:13:20 +0100

    Updated to new repo

--- cinst-gh.ps1
+++ cinst-gh.ps1
@@ -12,7 +12,7 @@
      iwr https://goo.gl/SZ9c3m | iex; cinst-gh furmark --force
 #>
 function cinst-gh {
-    $Repo = "https://github.com/majkinetor/au-packages/tree/master" 
+    $Repo = "https://github.com/C4illin/Choco-Packages/tree/master" 
 
     $name = $args[0]
     $download_page = iwr $Repo/$name -UseBasicParsing

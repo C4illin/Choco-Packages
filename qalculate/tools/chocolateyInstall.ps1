78d72870a8e874ac2e10dd27ad85a4e3ad4422c5
C4illin <c@illin.cf>
2018-11-04 20:03:45 +0100

    Updated

--- qalculate/tools/chocolateyInstall.ps1
+++ qalculate/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'qalculate'
   fileType               = 'MSI'
-  url                    = 'https://github.com/Qalculate/libqalculate/releases/download/v2.6.2/qalculate-2.6.2.msi'
-  checksum               = '19F8BF99AE72EB6DE4B91A5625667C331941CB5374B7B82741B1D8BDC4E55313'
+  url                    = 'https://github.com/Qalculate/libqalculate/releases/download/v2.8.0/qalculate-2.8.0.msi'
+  checksum               = ''
   checksumType           = 'sha256'
   silentArgs             = '/qn'
   validExitCodes         = @(0)

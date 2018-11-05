d32176d0a00bad9a5d38911a40b8fa52cd7ad9c5
C4illin <c@illin.cf>
2018-11-05 09:45:02 +0100

    Fixed one part of the timeout error

--- qalculate/update.ps1
+++ qalculate/update.ps1
@@ -12,7 +12,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $url     = $download_page.links | ? href -match '.msi$' | % href | select -First 1
 	$version = ($url -split '/' | select -last 1 -skip 1).substring(1)

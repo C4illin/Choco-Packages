b31edb4b07cb683b40c0d8d409366dacb0d4e357
C4illin <c@illin.cf>
2018-11-05 10:29:07 +0100

    Fixed Checksum error

--- qalculate/update.ps1
+++ qalculate/update.ps1
@@ -11,6 +11,10 @@ function global:au_SearchReplace {
     }
 }
 
+function global:au_BeforeUpdate() {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+}
+
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 

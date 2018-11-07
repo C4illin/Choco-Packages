cdeb1b828e5106e5b502ac5d8088705d1b5cdb89
Chocolatey <chocolatey@realdimensions.net>
2018-11-07 16:18:09 +0000

    AU: 1 updated - qalculate [skip ci] https://gist.github.com/227088b329dc2be21dbf7352d31af92b/ed73095e747f907121871c0063d4bdbd306f2f2f

--- qalculate/tools/chocolateyInstall.ps1
+++ qalculate/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'qalculate'
   fileType               = 'MSI'
-  url                    = 'https://github.com/Qalculate/libqalculate/releases/download/v2.8.0/qalculate-2.8.0.msi'
-  checksum               = '89368ca0d8920c10a4e96d2667dc2638c3436c0d9bf5d6523f21fd4bcb93ab23'
+  url                    = 'https://github.com/Qalculate/libqalculate/releases/download/v2.8.1/qalculate-2.8.1.msi'
+  checksum               = '0d1d3942773c1786bbffe9bcddfc08bdee51c859c0f3c41b5791088013e9296d'
   checksumType           = 'sha256'
   silentArgs             = '/qn'
   validExitCodes         = @(0)

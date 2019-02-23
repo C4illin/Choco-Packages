778f99bde4e94e8f14749b553656052e11fc8047
C4illin <c@illin.cf>
2019-02-23 18:49:52 +0100

    Added x64 version and changed releases URL

--- qalculate/tools/chocolateyInstall.ps1
+++ qalculate/tools/chocolateyInstall.ps1
@@ -3,10 +3,37 @@
 $packageArgs = @{
   packageName            = 'qalculate'
   fileType               = 'MSI'
-  url                    = 'https://github.com/Qalculate/libqalculate/releases/download/v2.9.0/qalculate-2.9.0.msi'
+  url                    = 'https://github.com/Qalculate/qalculate-gtk/releases/download/v2.9.0/qalculate-2.9.0.msi'
+  url64bit               = 'https://github.com/Qalculate/qalculate-gtk/releases/download/v2.9.0/qalculate-2.9.0-x64.msi'
   checksum               = 'c1a37cf7491d1eaae4192fdeb278878aa06d811b0ae1d8346fde894f62acbe96'
+  checksum64             = '800744753f52c8ce43d97e12b8b68c56b9d6de19ba6a4baf96347c1b3069f892'
   checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  softwareName           = 'Qalculate!*'
   silentArgs             = '/qn'
   validExitCodes         = @(0)
 }
+
+# Credits to chocolatey-coreteampackages
+[array]$key = Get-UninstallRegistrykey $packageArgs.softwareName
+if ($key.Count -eq 1) {
+  if ($key.DisplayVersion -eq '') {
+    Write-Host "Software already installed"
+    return
+  }
+  else {
+    Uninstall-ChocolateyPackage -packageName $packageArgs.packageName `
+      -fileType $packageArgs.fileType `
+      -silentArgs "$($key.PSChildName) $($packageArgs.silentArgs)" `
+      -validExitCodes $packageArgs.validExitCodes `
+      -file ''
+  }
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "This will most likely cause a 1603/1638 failure when installing InkScape."
+  Write-Warning "Please uninstall Qalculate! before installing this package."
+}
+
 Install-ChocolateyPackage @packageArgs
--- qalculate/update.ps1
+++ qalculate/update.ps1
@@ -1,30 +1,35 @@
 import-module au
 
-$releases = 'https://github.com/Qalculate/libqalculate/releases'
+$releases = 'https://github.com/Qalculate/qalculate-gtk/releases'
 
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
 function global:au_BeforeUpdate() {
     $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
 }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $url     = $download_page.links | ? href -match '.msi$' | % href | select -First 1
-	$version = ($url -split '/' | select -last 1 -skip 1).substring(1)
+    $url64  = $download_page.links | ? href -match '.msi$' | % href | select -First 1
+    $url32   = $url64 -replace '-x64.msi$', '.msi'
+	$version = ($url32 -split '/' | select -last 1 -skip 1).substring(1)
 
     @{
-        URL32   = $url
+        URL32   = $url32
+        URL64   = $url64
         Version = $version
     }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor all

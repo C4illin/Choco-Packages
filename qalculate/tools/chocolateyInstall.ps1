1c44da8bb83db5fda9402d93f63aa54664b2fdca
C4illin <c@illin.cf>
2019-09-01 23:43:59 +0200

    Removed uninstallation before installation

--- qalculate/tools/chocolateyInstall.ps1
+++ qalculate/tools/chocolateyInstall.ps1
@@ -14,26 +14,4 @@ $packageArgs = @{
   validExitCodes         = @(0)
 }
 
-# Credits to chocolatey-coreteampackages
-[array]$key = Get-UninstallRegistrykey $packageArgs.softwareName
-if ($key.Count -eq 1) {
-  if ($key.DisplayVersion -eq '') {
-    Write-Host "Software already installed"
-    return
-  }
-  else {
-    Uninstall-ChocolateyPackage -packageName $packageArgs.packageName `
-      -fileType $packageArgs.fileType `
-      -silentArgs "$($key.PSChildName) $($packageArgs.silentArgs)" `
-      -validExitCodes $packageArgs.validExitCodes `
-      -file ''
-  }
-}
-elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "This will most likely cause a 1603/1638 failure when installing Qalculate!"
-  Write-Warning "Please uninstall Qalculate! before installing this package."
-}
-
 Install-ChocolateyPackage @packageArgs

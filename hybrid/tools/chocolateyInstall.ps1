a88e20a7f3e800ac54351b8b67e216648ede65dc
C4illin <emrik.ostling@gmail.com>
2020-07-17 18:49:02 +0200

    disabled mpv.net and fixed Hybrid

--- hybrid/tools/chocolateyInstall.ps1
+++ hybrid/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName            = 'hybrid'
   fileType               = 'EXE'
-  url64                  = 'http://www.selur.de/sites/default/files/hybrid_downloads/Hybrid_2020.02.19.1_SETUP.exe'
+  url                  = 'http://www.selur.de/sites/default/files/hybrid_downloads/Hybrid_2020.02.19.1_SETUP.exe'
   checksum64             = '868b10322a401c998faa2181afe9a2840d49ecddc403ed3d77869e4a0d88b0a0'
   checksumType64         = 'sha256'
   softwareName           = 'Hybrid*'
--- hybrid/update.ps1
+++ hybrid/update.ps1
@@ -5,24 +5,24 @@ $releases = 'http://www.selur.de/downloads'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
 function global:au_BeforeUpdate {
-    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL
 }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $url64  = 'http://www.selur.de' + ($download_page.links | ? href -match '.exe$' | % href | select -First 1)
-	$version = ($url64 -split '/' | select -last 1) -split "_" | select -first 1 -skip 1
+    $url  = 'http://www.selur.de' + ($download_page.links | Where-Object href -match '.exe$' | ForEach-Object href | Select-Object -First 1)
+	$version = ($url -split '/' | Select-Object -last 1) -split "_" | Select-Object -first 1 -skip 1
 
     @{
-        URL64   = $url64
+        URL   = $url
         Version = $version
     }
 }

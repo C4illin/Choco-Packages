909b11ed72fc92011e2fe9abbdafb8bc9b225cd4
Chocolatey <chocolatey@realdimensions.net>
2021-02-09 00:13:48 +0000

    AU: 1 updated - th-ch-youtube-music [skip ci] https://gist.github.com/227088b329dc2be21dbf7352d31af92b

--- th-ch-youtube-music/tools/chocolateyInstall.ps1
+++ th-ch-youtube-music/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'th-ch-youtube-music'
   fileType               = 'EXE'
-  url                    = 'https://github.com/th-ch/youtube-music/releases/download/v1.9.0/YouTube-Music-1.9.0.exe'
-  checksum               = 'aaef355ea2f57f5a70126bbaf1dfcfc2f2f0cea407c002f863bd6da5a7bf1d65'
+  url                    = 'https://github.com/th-ch/youtube-music/releases/download/v1.10.0/YouTube-Music-1.10.0.exe'
+  checksum               = '31d1c194785c326305bd401594902c719b44e4f9ea0711e34fcbe4e5b02f9c67'
   checksumType           = 'sha256'
   softwareName           = 'YouTube Music*'
   silentArgs             = '/S'

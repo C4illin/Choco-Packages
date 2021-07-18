f7cb673800fc4174505596370f81a15cf2a2cfba
Chocolatey <chocolatey@realdimensions.net>
2021-07-18 00:14:49 +0000

    AU: 1 updated - hybrid [skip ci] https://gist.github.com/227088b329dc2be21dbf7352d31af92b/493ff60417738f9d4746cf9c4c70ede0f409e06d

--- hybrid/tools/chocolateyInstall.ps1
+++ hybrid/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'hybrid'
   fileType               = 'EXE'
-  url                    = 'http://www.selur.de/sites/default/files/hybrid_downloads/Hybrid_2021.04.12.1_SETUP.exe'
-  checksum               = '6b838a05071899b8c733f48eec3252e7032fc3c265f63523568d1d43f97a2103'
+  url                    = 'http://www.selur.de/sites/default/files/hybrid_downloads/Hybrid_2021.07.17.1_SETUP.exe'
+  checksum               = 'c0dd4b4d7c93d7a122d649ad58e63ba2ef7227b79bf2a47867eb82b976157295'
   checksumType           = 'sha256'
   softwareName           = 'Hybrid*'
   silentArgs             = '/VERYSILENT'

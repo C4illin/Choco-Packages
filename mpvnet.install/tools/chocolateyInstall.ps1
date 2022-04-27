4b0b30d1e4f970f40222088b35c59345636833a7
C4illin <emrik.ostling@gmail.com>
2022-04-27 02:25:30 +0200

    Update mpv.net

--- mpvnet.install/tools/chocolateyInstall.ps1
+++ mpvnet.install/tools/chocolateyInstall.ps1
@@ -3,10 +3,8 @@
 $packageArgs = @{
   packageName            = 'mpvnet.install'
   fileType               = 'EXE'
-  url                    = 'https://github.com/stax76/mpv.net/releases/download/5.4.8.0/mpv.net-setup-x64-5.4.8.0.exe'
-  url64bit               = 'https://github.com/stax76/mpv.net/releases/download/5.4.9.0/mpv.net-5.4.9.0-setup.exe'
-  checksum               = '2bada00b7b02bf4f067077e8b4f07e1dd7455bf41e1de397959a55eb18de6bd6'
-  checksum64             = 'd83a992146a67977d510342063a275a6af866350760af2cb0b6447b53ea48299'
+  url                    = 'https://github.com/stax76/mpv.net/releases/download/5.4.9.0/mpv.net-5.4.9.0-setup.exe'
+  checksum               = 'd83a992146a67977d510342063a275a6af866350760af2cb0b6447b53ea48299'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   softwareName           = 'mpv.net*'
--- mpvnet.install/update.ps1
+++ mpvnet.install/update.ps1
@@ -5,29 +5,24 @@ $releases = 'https://github.com/stax76/mpv.net/releases'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"   = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
         }
     }
 }
 
 function global:au_BeforeUpdate() {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
-    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+    $Latest.Checksum = Get-RemoteChecksum $Latest.Url
 }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $url64   = $download_page.links | ? href -match '.exe$' | % href | select -First 1
-    $url32   = $download_page.links | ? href -match '.exe$' | % href | select -First 1 -skip 1
-	$version = $url64 -split '/' | select -last 1 -skip 1
+    $url   = $download_page.links | ? href -match '.exe$' | % href | select -First 1
+	$version = $url -split '/' | select -last 1 -skip 1
 
     @{
-        URL32   = 'https://github.com' + $url32
-        URL64   = 'https://github.com' + $url64
+        URL   = 'https://github.com' + $url
         Version = $version
     }
 }

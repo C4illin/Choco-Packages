8a108586404e3601ec64066bf9ccb5a4497aef2b
C4illin <emrik.ostling@gmail.com>
2022-04-27 03:04:49 +0200

    added portable

---
+++ mpvnet.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName            = 'mpvnet.portable'
+  url                    = 'https://github.com/stax76/mpv.net/releases/download/v5.8.0.0/mpv.net-5.8.0.0-beta.zip'
+  checksum               = '683b4f86e2192f8d9ca729bc4992fab4b1f44b248a08eff3e0b5ef4ff22a18b2'
+  checksumType           = 'sha256'
+  unzipLocation          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ mpvnet.portable/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'https://github.com/stax76/mpv.net/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"   = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum = Get-RemoteChecksum $Latest.Url
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url   = $download_page.links | Where-Object href -match '.zip$' | ForEach-Object href | Select-Object -First 1
+	$version = ($url -split '/' | Select-Object -last 1 -skip 1) -replace 'v'
+
+    @{
+        URL   = 'https://github.com' + $url
+        Version = $version
+    }
+}
+
+update -ChecksumFor all

5f1e00c75accf6b0f0c30abbd2b9518c0f83a525
Chocolatey <chocolatey@realdimensions.net>
2022-06-05 16:13:50 +0000

    AU: 2 updated - mpv.net mpvnet.portable [skip ci] https://gist.github.com/227088b329dc2be21dbf7352d31af92b/dba86a5c348209aae057285446c6e53344841cdb

--- mpvnet.portable/tools/chocolateyInstall.ps1
+++ mpvnet.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName            = 'mpvnet.portable'
-  url                    = 'https://github.com/stax76/mpv.net/releases/download/v5.9.0.0-beta/mpv.net-5.9.0.0-beta.zip'
-  checksum               = 'f206e435ac9a41fffd865c35b9fdd89228f0a8787194d14fa9b432f33a94d735'
+  url                    = 'https://github.com/stax76/mpv.net/releases/download/v6.0.0.0-beta/mpv.net-6.0.0.0-beta.zip'
+  checksum               = 'dd33d10bcb4bbe69878453f5f72e97178397de159b2c8261d0fbd3532436aefc'
   checksumType           = 'sha256'
   unzipLocation          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }

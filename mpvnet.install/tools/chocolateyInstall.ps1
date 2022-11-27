c2deba25c9b4489a41468e98c065515b01c81133
C4illin <emrik.ostling@gmail.com>
2022-11-27 22:50:24 +0100

    changed url

--- mpvnet.install/tools/chocolateyInstall.ps1
+++ mpvnet.install/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName            = 'mpvnet.install'
   fileType               = 'EXE'
-  url                    = 'https://github.com/stax76/mpv.net/releases/download/5.4.9.0/mpv.net-5.4.9.0-setup.exe'
+  url                    = 'https://github.com/mpvnet-player/mpv.net/releases/download/5.4.9.0/mpv.net-5.4.9.0-setup.exe'
   checksum               = 'd83a992146a67977d510342063a275a6af866350760af2cb0b6447b53ea48299'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
--- mpvnet.portable/tools/chocolateyInstall.ps1
+++ mpvnet.portable/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName            = 'mpvnet.portable'
-  url                    = 'https://github.com/stax76/mpv.net/releases/download/v6.0.3.1/mpv.net-6.0.3.1-beta.zip'
+  url                    = 'https://github.com/mpvnet-player/mpv.net/releases/download/v6.0.3.1/mpv.net-6.0.3.1-beta.zip'
   checksum               = 'a3c78644cb19c559c5b73f148df4b8c80f6287d9b713ea93426c3d52ace52c20'
   checksumType           = 'sha256'
   unzipLocation          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"

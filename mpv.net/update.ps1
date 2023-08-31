54710b73549ffcf1f38104fc1b5f786d982ca328
C4illin <emrik.ostling@gmail.com>
2023-08-31 11:13:11 +0200

    fix mpv.net

--- mpv.net/update.ps1
+++ mpv.net/update.ps1
@@ -15,8 +15,8 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $urlcomp = $download_page.links | Where-Object href -match '/releases/tag/v' | ForEach-Object href | Select-Object -First 1
-    $version = ($urlcomp -split '/' | Select-Object -last 1) -replace 'v'
-    $url = ($urlcomp -replace '/tag/', '/download/') + '/mpv.net-' + $version + '.zip'
+    $version = ($urlcomp -split '/' | Select-Object -last 1) -replace 'v' -replace '-stable'
+    $url = ($urlcomp -replace '/tag/', '/download/') + '/mpv.net-v' + $version + '.zip'
 
     @{
         URL   = 'https://github.com' + $url

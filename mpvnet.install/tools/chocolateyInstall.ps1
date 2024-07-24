b4f5f65f72b4ee3a31d828851c595824cfc9ae8d
Chocolatey <chocolatey@realdimensions.net>
2024-07-24 16:42:39 +0000

    AU: 3 updated - mpv.net mpvnet.install mpvnet.portable [skip ci] https://gist.github.com/C4illin/227088b329dc2be21dbf7352d31af92b/7b9afd7c31658430c3e28bda98aa6687aa8a8dca

--- mpvnet.install/tools/chocolateyInstall.ps1
+++ mpvnet.install/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'mpvnet.install'
   fileType               = 'EXE'
-  url                    = 'https://github.com/mpvnet-player/mpv.net/releases/download/v7.1.1.0/mpv.net-v7.1.1.0-setup.exe'
-  checksum               = '11348ce74df3098c2b0e809121f1fb3d13ddd058b637094a5ecc44bc5107e0ca'
+  url                    = 'https://github.com/mpvnet-player/mpv.net/releases/download/v7.1.1.1-beta/mpv.net-v7.1.1.1-beta-setup-x64.exe'
+  checksum               = '210dec49da3ab467c6d6cf794c47f89117da18fdd74ebdf3bfb6b83bcdb91177'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   softwareName           = 'mpv.net*'
--- mpvnet.portable/tools/chocolateyInstall.ps1
+++ mpvnet.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName            = 'mpvnet.portable'
-  url                    = 'https://github.com/mpvnet-player/mpv.net/releases/download/v7.1.1.0/mpv.net-v7.1.1.0-portable.zip'
-  checksum               = '3c0f00e3fd57bfd6e118ddcd6e6dcfa9dfeb10290ac90d27ff355e0e16f61b12'
+  url                    = 'https://github.com/mpvnet-player/mpv.net/releases/download/v7.1.1.1-beta/mpv.net-v7.1.1.1-beta-portable-x64.zip'
+  checksum               = '08db067f8e12d7d5e2333ba952c92abae9ef5a39a80af0e609954e65b81f9bd6'
   checksumType           = 'sha256'
   unzipLocation          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }

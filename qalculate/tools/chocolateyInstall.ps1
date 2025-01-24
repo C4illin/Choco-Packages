c41fe69c345fd9ed77563b6d5505d784aed047d8
Peter J. Mello <admin@petermello.net>
2025-01-24 02:26:27 +0000

    Correct logging behavior introduced in 507a233
    This comment resolves issues relating to the creation
    of an install log in 507a233 that ran afoul of the
    rubric Chocolatey uses to parse those arguments
    (recorded in chocolatey/choco#813). In short:
    - Add functions to chocolateyInstall.ps1 to test the
      logging path and its ACL for writable status and try
      to correct them at runtime if insufficient.
    - Create a tools/chocolateyUninstall.ps1 script to
      explicitly handle package removal, including the
      removal of the qalc.exe CLI moved upon install.
    - Add SPDX licensing and copyright tags to all relevant
      files, just for the sake of being thorough.
    - Run all PowerShell scripts through PSScriptAnalyzer
      linter and resolve all errors and warnings.
    
    Signed-off-by: Peter J. Mello <admin@petermello.net>

--- qalculate/tools/chocolateyInstall.ps1
+++ qalculate/tools/chocolateyInstall.ps1
@@ -1,4 +1,58 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿# SPDX-FileCopyrightText: © 2018–2023, Emrik Östling <hi@emrik.org>
+# SPDX-FileCopyrightText: © 2024–2025, Peter J. Mello <admin@petermello.net>
+#
+# SPDX-License-Identifier: MIT
+
+$ErrorActionPreference = 'Stop'
+
+$chocoPkgInstallLogFile = (Join-Path -Path "${Env:TEMP}" `
+  -ChildPath "${packageName}-${Env:chocolateyPackageVersion}-MsiInstall.log")
+$chocoPkgInstallLogDir = (Split-Path "${chocoPkgInstallLogFile}" -Parent)
+
+function Test-PkgInstallLogDirWritable {
+  [CmdletBinding()]
+  [OutputType([Int])]
+  param([string]${Directory})
+  Try {
+    if ((Test-Path -Path "${Directory}") -and (
+      Test-Path -Path "${Directory}" -PathType Container -Access Write
+    )) {
+      Return 0
+    }
+  }
+  Catch {
+    Return 1
+  }
+}
+
+
+function Set-PkgInstallLogDirWritable {
+  [CmdletBinding(supportsShouldProcess)]
+  [OutputType([Int])]
+  param([string]${Directory})
+  Try {
+    if (-not (Test-Path -Path "${Directory}")) {
+      New-Item -Path "${Directory}" -ItemType Directory
+    }
+    $logDirAcl = Get-Acl -Path "${Directory}"
+    $logDirRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
+      "Everyone", "Write", "Allow"
+    )
+    ${logDirAcl}.SetAccessRule(${logDirRule})
+    Set-Acl -Path "${Directory}" -AclObject "${logDirAcl}"
+    Return 0
+  }
+  Catch {
+    Return 1
+  }
+}
+
+if (Test-PkgInstallLogDirWritable -Directory "${chocoPkgInstallLogDir}") {
+  Write-Information "Logging directory exists and is writable; proceeding with installation…"
+} elseif (Set-PkgInstallLogDirWritable -Directory "${chocoPkgInstallLogDir}") {
+  Write-Error "Unable to change logging directory permissions; aborting…"
+  Exit 1
+}
 
 $packageArgs = @{
   packageName            = 'qalculate'
@@ -10,7 +64,7 @@ $packageArgs = @{
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   softwareName           = 'Qalculate!*'
-  silentArgs             = '/qn /norestart /l*v `"$($Env:TEMP)\$($packageName).$($Env:chocolateyPackageVersion).MsiInstall.log`"'
+  silentArgs             = "/qn /norestart /l*v ${chocoPkgInstallLogFile}"
   validExitCodes         = @(0)
 }
 
